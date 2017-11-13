29a26b95069ba17a72109fa95ac6d4c8ecbe5d5f
Holly N Little <hollyisalwayshopeful@gmail.com>
2017-11-13 00:15:02 -0500

    refactoring

--- FortuneTeller/Program.cs
+++ FortuneTeller/Program.cs
@@ -12,119 +12,144 @@ namespace FortuneTeller
         {
             //Ask user questions. 
             //Assign user's answers to variables.
-            Console.Write("Enter first name: ");
-            string firstName = Console.ReadLine();
-            Console.Write("\nEnter last name: ");
-            string lastName = Console.ReadLine();
-            Console.Write("\nEnter your age: ");
-            int age = Convert.ToInt32(Console.ReadLine());
-            Console.Write("\nEnter the digit of your birth month: ");
-            int birthMonth = Convert.ToInt32(Console.ReadLine());
-
-            //Ask user for to select a favorite ROYGBIV color.
-            //Ask user to type help if need a list of colors.
             //Use ToLower method to ensure case insensitivity.
-            string color;
-            Console.WriteLine("\nWhat is your favorite ROYGBIV color?");
-            Console.WriteLine("\nEnter HELP to get a list of the ROYGBIV colors, if needed.");
-            string answer = Console.ReadLine().ToLower();
-            if (answer == "help")
+            String firstName;
+            String lastName;
+            int age;
+            int birthMonth;
+            String favoriteColor;
+            int numberOfSiblings;
+            int yearsToRetire;
+            string vacationLocation;
+            int accountBalance;
+            string transportation;
+            Console.WriteLine("\n()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()\n");
+            Console.WriteLine("                               FORTUNE TELLER\n\n");
+            Console.WriteLine("()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()");
+            Console.WriteLine
+                ("\n  Hello there! \n\n  My name is Fortune Teller.  \n  Would you like to answer a few questions for me, \n  then I will tell you what I see in your future! ");
+            Console.Write("  \n  Would you like that?  Please type yes or no?  ");
+            string play = Console.ReadLine().ToLower();
+
+            if (play != "yes")
             {
-                Console.WriteLine("\nROYGBIV Colors; Red, Orange, Yellow, Green, Blue, Indigo, Violet");
-                Console.Write("\nColor? ");
-                color = Console.ReadLine();
+                Console.WriteLine("  Goodbye! ");
+                Console.ReadKey();
+            }
+            Console.Write("  Great!  What is your first name?  ");
+            firstName = Console.ReadLine();
+            Console.Write("\n  What is your last name?  ");
+            lastName = Console.ReadLine();
+            Console.Write
+                ("\n  Hello {0} {1} , thank you for playing!  \n\n  How old are you?  ", firstName, lastName);
+            age = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("\n  What is the digit of your birth month?  ");
+            birthMonth = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\n  What ROYGBIV color do you like the most?  ");
+            Console.Write("\n  Would you like me to list the ROYGBIV colors?  If so, enter HELP, if not enter no thanks");
+            string help = Console.ReadLine().ToLower();
+            if (help == "help")
+            {
+                Console.WriteLine
+                    ("\n     RED\n     ORANGE\n     YELLOW\n     GREEN\n     BLUE\n     INDIGO\n     VIOLET\n     Which color is your favorite?");
             }
             else
             {
-                color = Console.ReadLine();
+                Console.WriteLine("\n     Great!");
             }
-            Console.Write("\nHow many siblings do you have? ");
-            int siblings = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\n  Please enter your favorite color.  ");
+            favoriteColor = Console.ReadLine().ToLower();
+            Console.Write("\n  One last question, how many siblings do you have?  ");
+            numberOfSiblings = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\n  Thank you {0}{1} so much for answering all of my questions! \n", (firstName),(lastName));
+
 
             //Assign fortune information based on the user's answers.
-            //Assign years to retire based on whether or not the user's age is even.
-            int retire;
-            int ageDigit = Convert.ToInt32(age);
-
-            if (age % 2 != 0)
+            //The number of years to retire is based on whether or not the user's age is even.
+            if (age % 2 == 0)
             {
-                retire = 10;
+                yearsToRetire = 10;
             }
             else
-                retire = 20;
+            {
+                yearsToRetire = 100;
+            }
 
-            //Assign mode of transportation based on number of siblings.
-            string vacationHome;
-            if (siblings == 0)
+            //The vacation house location is based on the user's number of siblings.
+            switch (numberOfSiblings)
             {
-                vacationHome = "Coronado, Panama";
-            }
-            else if (siblings == 1)
-            {
-                vacationHome = "Cotacachi, Ecuador";
-            }
-            else if (siblings == 2)
-            {
-                vacationHome = "Central Valley, Costa Rica";
-            }
-            else if (siblings == 3)
-            {
-                vacationHome = "Cali, Colombia";
-            }
-            else if (siblings > 3)
-            {
-                vacationHome = "Cancun, Mexico";
-            }
-            else vacationHome = "Avon Lake, Ohio";
-
-            //Use the user's color choice to assign a mode of transportation.
-            string transportMode;
-            switch (color)
-            {
-                case "Red":
-                    transportMode = "fast car";
+                case 0:
+                    vacationLocation = "Montego Bay, Jamaica";
                     break;
-                case "Orange":
-                    transportMode = "private jet";
+                case 1:
+                    vacationLocation = "Montreal, Canada";
                     break;
-                case "Yellow":
-                    transportMode = "stretch limo";
+                case 2:
+                    vacationLocation = "Mekong, Cambodia";
                     break;
-                case "Green":
-                    transportMode = "private plane";
-                    break;
-                case "Blue":
-                    transportMode = "helicopter";
-                    break;
-                case "Violet":
-                    transportMode = "luxury motorhome";
+                case 3:
+                    vacationLocation = "Monterey, California";
                     break;
                 default:
-                    transportMode = "motorcycle";
+                    vacationLocation = "Cancun, Mexico";
                     break;
             }
 
-            //Assign the user's bank account balance based on birth month.
-            int money;
-            if (birthMonth > 1 && birthMonth < 5)
+            //The mode of transportation is based on the user's favorite color.
+            switch (favoriteColor)
             {
-                money = 900000;
-            }
-            else if (birthMonth < 6 && birthMonth < 9)
-            {
-                money = 800000;
-            }
-            else if (birthMonth > 8 && birthMonth < 13)
-            {
-                money = 700000;
-            }
-            else
-                money = 0;
+                case "red":
+                    transportation = "car";
+                    break;
+                case "orange":
+                    transportation = "plane";
+                    break;
+                case "yellow":
+                    transportation = "boat";
+                    break;
+                case "green":
+                    transportation = "limousine";
+                    break;
+                case "blue":
+                    transportation = "bike";
+                    break;
+                case "indigo":
+                    transportation = "helicopter";
+                    break;
+                case "violet":
+                    transportation = "private jet";
+                    break;
+                default:
+                    transportation = "RV";
+                    break;
 
+            }
+
+            //The bank account balance amout is based on the user's birth month.
+            accountBalance = 100;
+            if (age >= 1  && age <= 4)
+            {
+                accountBalance = 10;
+            }
+            else if (age >= 5 && age <= 9)
+            {
+                accountBalance = 100;
+            }
+            else if (age >= 10 && age <= 12)
+            {
+                accountBalance = 50;
+            }
+            else 
+            {
+                accountBalance = 0;
+            }
+            
             //Print the user's fortune.
-            Console.WriteLine("\n" + firstName + " " + lastName + " will retire in " + retire +
-                " years " + "with " + money + " in the bank, \na vacation home in " +
-                vacationHome + " and a " + transportMode + "!");
+            Console.WriteLine
+                ("\n\n  Congratulations, {0} {1}!  \n    I see that You will retire in {2} years, \n    with ${3} in the bank! \n    You will have a grand vacation home in {4}. \n    and use a {5} to get around town.", (firstName),(lastName),(yearsToRetire),(accountBalance),(vacationLocation),(transportation));
+
+            Console.ReadKey();
         }
     }
 }
